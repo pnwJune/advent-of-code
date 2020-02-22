b9622b04a3728e5d7c6f2b02373b8a7aed3130d9
June Ma <june.liang.ma@gmail.com>
2020-02-22 12:40:37 -0800

    refactor execute method

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -14,18 +14,22 @@ namespace AdventOfCode
 			string command = args[0];
 			if (command.ToLower() == "fuelcounter")
 			{
-				string inputPath = args[1];
-				int sum = 0;
-				foreach (string line in File.ReadLines(inputPath))
-				{
-					if (int.TryParse(line, out int mass))
-					{
-						sum += FuelCounter.FuelCounter.CalculateFuel(mass);
-					}
-				}
-
-				Console.WriteLine($"Sum of input fuel counters: {sum}");
+				Program.ExecuteFuelCounter(args[1]);
 			}
+		}
+
+		private static void ExecuteFuelCounter(string inputPath)
+		{
+			int sum = 0;
+			foreach (string line in File.ReadLines(inputPath))
+			{
+				if (int.TryParse(line, out int mass))
+				{
+					sum += FuelCounter.FuelCounter.CalculateFuel(mass);
+				}
+			}
+
+			Console.WriteLine($"Sum of input fuel counters: {sum}");
 		}
 	}
 }
